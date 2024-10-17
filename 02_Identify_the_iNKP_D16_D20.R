050595297d605bee7f9a6c51533425639064c2af
Lin Yunqing <61617978+lyqbiocc@users.noreply.github.com>
2024-10-17 09:01:21 +0800

    Update and rename Identify_the_iNKP_D16_D20.R to 02_Identify_the_iNKP_D16_D20.R

--- 02_Identify_the_iNKP_D16_D20.R
+++ 02_Identify_the_iNKP_D16_D20.R
@@ -1,4 +1,14 @@
 #subset Hematopoietic_progenitor and iNK lineage cells to second-analysis  
+## library packages
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(viridis)
+library(ggplot2)
+library(magrittr)
+library(pheatmap)
+library(clusterProfiler)
+library(cowplot) #combine plots
 
 D16_D20.seu <- readRDS("subsets_D16_D20_anno3.rds")
 
@@ -7,38 +17,23 @@ CD45_CD34_imNK=subset(D16_D20.seu,cells=c(
   WhichCells(subset(D16_D20.seu,subset=anno_2=="iNK lineage"))))
 
 ## identify the highly various genes
-
-
 all.features=rownames(CD45_CD34_imNK)[(rowSums(CD45_CD34_imNK@assays$RNA@counts>0)>6)] #cell_num*0.05%
 CD45_CD34_imNK=subset(CD45_CD34_imNK,features=all.features)
 CD45_CD34_imNK <- FindVariableFeatures(CD45_CD34_imNK, selection.method = "vst", nfeatures = 2000)
-
-
-
 ## scale data to do dimension reduction analysis
 
 CD45_CD34_imNK <- ScaleData(CD45_CD34_imNK, features = VariableFeatures(object = CD45_CD34_imNK))
 
 ## do PCA analysis
 #backgroud gene list is the HVGs, which can obtain by VariableFeatures()  
-
 CD45_CD34_imNK <- RunPCA(CD45_CD34_imNK, features = VariableFeatures(object = CD45_CD34_imNK))
-
 ElbowPlot(CD45_CD34_imNK,ndims = 50)
 xx <- cumsum(CD45_CD34_imNK[["pca"]]@stdev^2)
 xx <- xx / max(xx)
 which(xx > 0.9)
-
-
 ## do UMAP, and dimmension plot for cell clusters  
-
 ## parameter 'dims': the number of dimension of PCA used to umap analysis
 CD45_CD34_imNK=FindNeighbors(CD45_CD34_imNK, dims = 1:30)
-CD45_CD34_imNK=FindClusters(CD45_CD34_imNK, resolution = 0.1, algorithm = 1)
-CD45_CD34_imNK=FindClusters(CD45_CD34_imNK, resolution = 0.2, algorithm = 1)
-CD45_CD34_imNK=FindClusters(CD45_CD34_imNK, resolution = 0.3, algorithm = 1)
-CD45_CD34_imNK=FindClusters(CD45_CD34_imNK, resolution = 0.4, algorithm = 1)
-CD45_CD34_imNK=FindClusters(CD45_CD34_imNK, resolution = 0.5, algorithm = 1)
 CD45_CD34_imNK=FindClusters(CD45_CD34_imNK, resolution = 0.6, algorithm = 1)
 CD45_CD34_imNK <- RunUMAP(CD45_CD34_imNK, dims = 1:30,return.model=T)
 
@@ -46,10 +41,7 @@ CD45_CD34_imNK <- RunUMAP(CD45_CD34_imNK, dims = 1:30,return.model=T)
 # plot the UMAP result of all cells with DimPlot
 ## parameter 'pt.size': the point size in UMAP plot
 p=plot_grid(
-  DimPlot(CD45_CD34_imNK, reduction = "umap", label = T,label.size = 5,group.by = "RNA_snn_res.0.1",raster = F),
   DimPlot(CD45_CD34_imNK, reduction = "umap", label = T,label.size = 5,group.by = "RNA_snn_res.0.6",raster = F),
-  DimPlot(CD45_CD34_imNK, reduction = "umap", label = T,label.size = 5,group.by = "RNA_snn_res.0.5",raster = F),
-  DimPlot(CD45_CD34_imNK, reduction = "umap", label = T,label.size = 5,group.by = "RNA_snn_res.0.4",raster = F),
   DimPlot(CD45_CD34_imNK, reduction = "umap", label = T,label.size = 5,group.by = "anno_2",raster = F),
   DimPlot(CD45_CD34_imNK, reduction = "umap", label = T,label.size = 5,group.by = "times",raster = F),
   ncol = 3
@@ -61,8 +53,6 @@ p=FeaturePlot(CD45_CD34_imNK,features = c("PTPRC","CD34","CD38","CD7","MME","IL2
 p
 
 p=DimPlot(CD45_CD34_imNK, reduction = "umap", label = T,label.size = 3,group.by = "anno_2",raster = F,split.by = "times",ncol = 5)
-p
-
 Idents(CD45_CD34_imNK)=CD45_CD34_imNK$RNA_snn_res.0.6
 CD45_CD34_imNK=RenameIdents(CD45_CD34_imNK,
                             "4"="e-iNKP",
@@ -74,44 +64,25 @@ CD45_CD34_imNK=RenameIdents(CD45_CD34_imNK,
                             "5"="other-iHPC","7"="other-iHPC","8"="other-iHPC","9"="other-iHPC","10"="other-iHPC","11"="other-iHPC")
 
 CD45_CD34_imNK$anno_3=as.vector(Idents(CD45_CD34_imNK))
-
-
 ## mapping with natural NKP and NK cells  
-
 ### load natural cells gene expression (STRT-seq)
-
 natural_immcell=read.delim("./GSE149938_umi_matrix.csv",sep=",",header = T)
-
-dim(natural_immcell)
-
-#rownames(natural_immcell)[grep(rownames(natural_immcell),pattern = "NKP")]
 meta=reshape2::colsplit(rownames(natural_immcell),"[_]",names=c("group","orig","bc"))
 NC_cells <- CreateSeuratObject(counts = t(natural_immcell),meta.data = meta,project = "natural_blood_cells", min.cells = 5, min.features = 100)
 #rm(data.seu)
-gc()
-
 # global expression of scRNA-seq 
 # The [[ operator can add columns to object metadata. This is a great place to stash QC stats
 NC_cells[["percent.mt"]] <- PercentageFeatureSet(NC_cells, pattern = "^MT-")
-
 P=VlnPlot(NC_cells, group.by = "group",features = c("nFeature_RNA", "nCount_RNA","percent.mt"), ncol = 3,pt.size = 0,raster = F)&labs(x="",y="")
-
-P
-
 Idents(NC_cells)=NC_cells$group
 NK_cells=subset(NC_cells,idents=c("NKP","kineNK","toxiNK"))
 rm(natural_immcell,meta,NC_cells)
 gc()
-
 NK_cells=NormalizeData(NK_cells,normalization.method = "LogNormalize", scale.factor =median(NK_cells$nCount_RNA))
-
-
 ### mapping with natural NKP and NK cells  
-
 #### sctransform before IntegrateData  (finalized fig)  
 # run SCTransform on each object separately
 iNKP_obj=SCTransform(CD45_CD34_imNK, verbose = FALSE)
-
 features1=rownames(NK_cells)[rowSums(NK_cells@assays$RNA@counts>0)>0]
 NK_cells=subset(NK_cells,features=features1)
 #NK_cells=subset(NK_cells,subset=group1=="NKP"|group1=="NK")
@@ -131,7 +102,6 @@ gc()
 DefaultAssay(pb.integrated) <- "integrated"
 pb.integrated <- RunPCA(pb.integrated, verbose = FALSE)
 ElbowPlot(pb.integrated,ndims = 30)
-
 pb.integrated <- RunUMAP(pb.integrated, dims = 1:25,n.neighbors = 30)
 
 
